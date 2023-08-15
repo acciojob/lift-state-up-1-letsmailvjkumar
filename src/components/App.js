911ed87121239f16b7c5c34588f2d625ab84531f
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-08-15 11:32:17 +0000

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  function Parent() {
+    const [showModal, setShowModal] = useState(false);
 
-export default App
+    return (
+      <div className="parent">
+        <h1>Parent Component</h1>
+        <Child showModal={showModal} setShowModal={setShowModal} />
+      </div>
+    );
+  }
+
+  function Child({ showModal, setShowModal }) {
+    return (
+      <div className="child">
+        <h2>Child Component</h2>
+        <button onClick={() => setShowModal(!showModal)}>Show Modal</button>
+        {showModal && (
+          <div className="modal">
+            <h3>Modal Content</h3>
+            <p>This is the modal content.</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return <Parent />;
+};
+
+export default App;
